0ae2eac9606704a3f90811cb43538c2d0ee24baa
kishannk10 <kishannk000@gmail.com>
2024-12-15 20:52:25 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,24 @@
+/*
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle. onclick = () => {
+        navLinks.classList.toggle('active');
+
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
